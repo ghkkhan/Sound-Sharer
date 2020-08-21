774576a3e6bf24bdd91c1f406c5189f279850477
ianhudson87 <46661041+ianhudson87@users.noreply.github.com>
2020-08-21 14:47:41 -0500

    set up socket stuff

--- client/index.js
+++ client/index.js
@@ -2,8 +2,8 @@
 var joinBool = false;
 
 
-var room_name = "room"
-var user_name = "user"
+var room_name = "room";
+var user_name = "user";
 
 creating = () => {
     document.getElementById("create_button").className = "switch disabled";
@@ -25,3 +25,31 @@ enter = () => {
     //the data is stored... send to server-side through socket or something...
     // change page to either the newly created room or go to an existing room...
 }
+
+
+///////////////////////////////////////// SOCKET STUFF
+var socket = io.connect();
+
+test = document.getElementById('create_button');
+test.addEventListener('click', ()=>{
+    console.log('hi')
+    socket.emit('test')
+})
+
+// create room handler
+
+
+function create_room(rName, uName){
+    socket.emit('create_room', {
+        rName: rName,
+        uName: uName
+    });
+};
+
+// join room handler
+function join_room(rName, uName){
+    socket.emit('join_room', {
+        rName: rName,
+        uName: uName
+    });
+};
