9624ab408b8d730be3c592f69af624de0a73f5ac
ianhudson87 <46661041+ianhudson87@users.noreply.github.com>
2020-08-25 20:02:39 -0500

    Revert "Revert "Merge branch 'master' of https://github.com/ghkkhan/Sound-Sharer""
    This reverts commit 15972a180c597e9f957f062c25208de82ee84803.

--- models/user.js
+++ models/user.js
@@ -1,13 +1,13 @@
-// https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/mongoose
-// Require Mongoose
-var mongoose = require('mongoose');
+// // https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/mongoose
+// // Require Mongoose
+// var mongoose = require('mongoose');
 
-// Define a schema
-var Schema = mongoose.Schema;
+// // Define a schema
+// var Schema = mongoose.Schema;
 
-var User = new Schema({
-    name: String
-});
+// var User = new Schema({
+//     name: String
+// });
 
-// Export function to create "User" model class
-module.exports = mongoose.model('User', User);
+// // Export function to create "User" model class
+// module.exports = mongoose.model('User', User);
